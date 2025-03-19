d105a7cd4f721acb54def7d681a441e6559b50a0
abhiineat <abhii.neat@gmail.com>
2025-03-19 14:57:14 +0530

    jwt

--- src/models/user.js
+++ src/models/user.js
@@ -45,4 +45,27 @@ userSchema.methods.isPasswordMatch = async function(password){
     return await bcrypt.compare(password,this.password);
 }
 
+userSchema.methods.generateToken = function(){
+    return jwt.sign({
+        _id:this._id,
+        username:this.username,
+        email:this.email,   
+
+    },
+process.env.ACCESS_TOKEN_SECRET,
+{
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+})
+}
+
+userSchema.methods.generateToken = function(){
+    return jwt.sign({
+        _id:this._id,
+
+    },
+process.env.REFRESH_TOKEN_SECRET,
+{
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+})
+}
 export  const User = mongoose.model("User", userSchema);
