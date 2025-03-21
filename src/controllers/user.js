afd95fce9ec2d63d17f17dbe598fdb608ee93f87
abhiineat <abhii.neat@gmail.com>
2025-03-21 19:21:28 +0530

    add register controller

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,7 +1,41 @@
 import { asynchandle } from "../utils/asynchandler.js";
-
+import { User } from "../models/user.js";
+import {uploadImage} from "../utils/cloudinary.js";
 const register = asynchandle(async (req, res) => {
-    res.status(200).json({message:"welldone"});
+   const {username, email, fullname,password}=req.body 
+    if(fullname.trim()===""){
+        res.status(400).json({message:"fullname is required"})
+    }
+    if(username.trim()===""){
+        res.status(400).json({message:"username is required"})
+    }
+    if(email.trim()===""){
+        res.status(400).json({message:"email is required"})
+    }
+    const existedUser=User.findOne({
+        $or:[
+        {username},{email}
+    ]
+    })
+    if(existedUser){
+        res.status(409).json({message:"username or email already existed"})
+    }
+    const avatarLocalPath=req.files?.avatar[0].path ;
+    if(!avatarLocalPath){
+        res.status(400).json({message:"avatar is required"})
+    }
+    const avtar=await uploadImage(avatarLocalPath);
+    if(!avtar){
+        res.status(500).json({message:"avatar upload failed"})
+    }
+    const user=await User.create({
+        username,email,fullname,password,avatar:avtar.url
+    })
+    const createdUser=await User.findById(user._id).select("-password -refreshToken" );
+    if(!createdUser){
+        res.status(500).json({message:"user creation failed"})
+    }
+    res.status(201).json({message:"user created successfully"})
 }
 );
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import { register } from "../controllers/user.js";
+import { upload } from "../middlewares/multer.js";
 const userRouter = Router();
-userRouter.route('/register').post(register);
+userRouter.route('/register').post(
+    upload.fields([{name:"avtar",maxCount:1}]),
+    register
+);
 
 
 export { userRouter };
