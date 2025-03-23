a1b264e9ce7e6a9281c8e9d50319590d72111dd1
abhiineat <abhii.neat@gmail.com>
2025-03-23 15:12:19 +0530

    unique password and username  error resolved

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -3,18 +3,23 @@ import { User } from "../models/user.js";
 import { uploadImage } from "../utils/cloudinary.js";
 
 const register = asynchandle(async (req, res) => {
-    console.log("Uploaded Files:", req.files);
-
     if (!req.files?.avatar?.[0]) {
         return res.status(400).json({ message: "Avatar is required" });
     }
 
     const avatarLocalPath = req.files.avatar[0].path;
-
     const avatarUpload = await uploadImage(avatarLocalPath);
 
     if (!avatarUpload?.url) {
         return res.status(500).json({ message: "Avatar upload failed" });
+    }
+    if(!req.body.password){
+        return res.status(400).json({ message: "Password is required" });
+    }
+
+    const existingUser = await User.findOne({ username: req.body.username });
+    if (existingUser) {
+        return res.status(400).json({ message: "Username is already taken" });
     }
 
     const user = await User.create({
@@ -32,5 +37,8 @@ const register = asynchandle(async (req, res) => {
 
     res.status(201).json({ message: "User created successfully", user: createdUser });
 });
+
+
+
 
 export { register };
